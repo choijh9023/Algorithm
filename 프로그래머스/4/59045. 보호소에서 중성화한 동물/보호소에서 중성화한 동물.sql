1fd8162656f49cf759e8847e6369407956f6c221
choi moon seok <148327627+choijh9023@users.noreply.github.com>
2024-02-20 16:05:46 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+SELECT i.ANIMAL_ID,i.ANIMAL_TYPE,i.NAME
+from ANIMAL_INS i
+join ANIMAL_OUTS o
+on i.animal_id = o.animal_id
+where i.SEX_UPON_INTAKE like '%intact%' and o.SEX_UPON_OUTCOME not like '%intact%'

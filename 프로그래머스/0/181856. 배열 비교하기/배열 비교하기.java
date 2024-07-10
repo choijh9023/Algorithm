c6dd5136f477f1b7d0325a5aeaa8af0c12d92021
choi moon seok <148327627+choijh9023@users.noreply.github.com>
2024-07-11 00:02:35 +0900

    [level 0] Title: 배열 비교하기, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -1,33 +1,24 @@
 class Solution {
     public int solution(int[] arr1, int[] arr2) {
-       int answer = 0;
-      if(arr2.length>arr1.length){
-          answer = -1;
-      }
+        if (arr2.length > arr1.length) {
+            return -1;
+        }
         
+        if (arr2.length < arr1.length) {
+            return 1;
+        }
         
-         if(arr2.length<arr1.length){
-          answer = 1;
-      }
         int arr1Sum = 0;
         int arr2Sum = 0;
-        
-        
-        if(arr1.length==arr2.length){
-            for(int i = 0; i<arr1.length; i++){
-                arr1Sum+=arr1[i];
-                arr2Sum+=arr2[i];        
-            }
-            if(arr1Sum==arr2Sum){
-                answer = 0 ;
-            }else if(arr2Sum>arr1Sum){
-                answer = -1; 
-            }else{
-                answer = 1;
-            }
-         
+        for (int i = 0; i < arr1.length; i++) {
+            arr1Sum += arr1[i];
+            arr2Sum += arr2[i];
         }
-       
-        return answer;
+        
+        if (arr1Sum == arr2Sum) {
+            return 0;
+        }
+        
+        return arr2Sum > arr1Sum ? -1 : 1;
     }
 }

48c99809b2876c8615c89b457c740cebb7ccaf37
choi moon seok <148327627+choijh9023@users.noreply.github.com>
2024-08-03 14:45:39 +0900

    [level 0] Title: 배열에서 문자열 대소문자 변환하기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.py
+++ 프로그래머스/0/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.py
@@ -7,3 +7,5 @@ def solution(strArr):
             answer.append(word.lower())
         
     return answer
+# def solution(strArr):
+#     return [s.lower() if i % 2 == 0 else s.upper() for i, s in enumerate(strArr)]

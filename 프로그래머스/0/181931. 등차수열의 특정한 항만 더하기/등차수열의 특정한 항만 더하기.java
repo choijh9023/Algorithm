5b1ad2aef972c913b89291ac8f1b66fdfd9b7399
choi moon seok <148327627+choijh9023@users.noreply.github.com>
2025-08-23 15:48:43 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.02 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int a, int d, boolean[] included) {
+        int answer = 0;
+        
+        for(int i = 0 ; i <included.length;i++){
+            if(i>0){
+                a += d;
+            
+                
+            }
+            if(included[i]){
+                answer +=a;
+            }
+
+        }
+        
+        
+        return answer;
+    }
+}

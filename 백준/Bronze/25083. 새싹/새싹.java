d7b545a7a409c8a953197b36955496ebbda49ecb
choi moon seok <148327627+choijh9023@users.noreply.github.com>
2025-10-14 20:53:22 +0900

    [Bronze V] Title: 새싹, Time: 96 ms, Memory: 13892 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,17 @@
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String []args){
+
+
+
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
